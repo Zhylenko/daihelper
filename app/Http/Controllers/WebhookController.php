f5906905da65cb556460b356f755b7d8594013c9
Vlad <zhylenko.vlad@gmail.com>
2022-02-08 22:29:52 +0200

    beta v1.1

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -11,11 +11,13 @@ class WebhookController extends Controller
 {
     public function index(Request $request)
     {
-        $update         = $request->all();
-        $message        = $update['message'];
-        $chatId         = $message['chat']['id'];
+        $update     = $request->all();
+        $message    = $update['message'];
+        $chatId     = $message['chat']['id'];
 
-        Telegram::sendMessage(['chat_id' => $chatId, 'text' => Exam::getAnswer($message['text'])]);
+        $answer     = Exam::getAnswer($message['text']);
+
+        Telegram::sendMessage(['chat_id' => $chatId, 'text' => $answer]);
 
         return '';
     }
